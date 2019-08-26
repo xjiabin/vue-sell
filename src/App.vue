22c2bcfa41a5cc925b3212cc90e446705d93d8c4
jiabin <18344164480@163.com>
2019-08-26 11:58:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <div class="header">
+            header
+        </div>
+        <div class="tab">
+            tab
+        </div>
+        <div class="content">
+            content
+        </div>
+    </div>
+</template>
+
+<style lang="stylus">
+</style>
