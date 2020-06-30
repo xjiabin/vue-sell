338bdd30eaaba3de41aa8738c83a608f9490a8c6
jiabin <18344164480@163.com>
2020-06-30 14:36:31 +0800

    更换域名

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const urlMap = {
     development: '/',
-    production: 'http://sell.xjiabin.cn/'
+    production: 'http://sell.jiabin.gold/'
 }
 const baseUrl = urlMap[process.env.NODE_ENV]
 const ERR_OK = 0
